5ae2b2108d74d6fb4c3baa7f1e6559806dad6875
Dicky Darmawan <84900035+dickyd9@users.noreply.github.com>
2026-09-05 14:59:12 +0700

    refactor(docs): remove standalone component lab launcher

--- apps/docs/src/main.tsx
+++ apps/docs/src/main.tsx
@@ -4,11 +4,9 @@ import '@digvation/ui/styles.css';
 import './docs.css';
 import './component-lab.css';
 import { App } from './App';
-import { ComponentLab } from './component-lab';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ComponentLab />
   </React.StrictMode>,
 );
