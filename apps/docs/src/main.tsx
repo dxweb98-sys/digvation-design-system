69d6d338e20373261e658bb9bb6203cb96fa7bb1
Dicky Darmawan <84900035+dickyd9@users.noreply.github.com>
2026-09-05 13:51:57 +0700

    feat(docs): mount interactive component lab

--- apps/docs/src/main.tsx
+++ apps/docs/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@digvation/ui/styles.css';
 import './docs.css';
+import './component-lab.css';
 import { App } from './App';
+import { ComponentLab } from './component-lab';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ComponentLab />
   </React.StrictMode>,
 );
